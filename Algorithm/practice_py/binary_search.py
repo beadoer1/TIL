33c6fd6aedb25c5676b6cfee4dd8143593265c0e
Youn-Hwan <beadoer1@gmail.com>
2021-01-21 14:04:59 +0900

    자료구조와 알고리즘 공부 시작 'Hello Coding 그림으로 개념을 이해하는 알고리즘'
    1. 알고리즘 개념과 기본 용어 정리
    2. 책에 그림이 많아서 좋다.

---
+++ Algorithm/practice_py/binary_search.py
@@ -0,0 +1,22 @@
+import math
+
+def binary_search(list,item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = math.floor((low + high) / 2)
+        guess = list[mid]
+        
+        if guess == item :
+            return mid
+        elif guess > item :
+            high = mid - 1
+        else :
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 8]
+
+print(binary_search(my_list,6))
+print(binary_search(my_list,7))
